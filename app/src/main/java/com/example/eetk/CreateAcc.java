29d003d23f0ff6fda4c653a54f7c2abb3d70aa88
SSAXXARR <SSAXXARR@yandex.ru>
2022-06-09 16:24:05 +0500

    не работает, но красиво2

---
+++ app/src/main/java/com/example/eetk/CreateAcc.java
@@ -0,0 +1,4 @@
+package com.example.eetk;
+
+public class CreateAcc {
+}
---
+++ app/src/main/java/com/example/eetk/Menu.java
@@ -0,0 +1,18 @@
+package com.example.eetk;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ScaleDrawable;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+    }
+}
